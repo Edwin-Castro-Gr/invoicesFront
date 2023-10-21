0b2a6128dd5d345beb5e735c14a488552f814b5e
Nelson Andrés Mora López <na2003ml.am@gmail.com>
2023-10-21 00:20:08 -0500

    Final build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { TokenProvider } from "./utils/tokenContext";
-
+import "bootstrap/dist/css/bootstrap.min.css"
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
