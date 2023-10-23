7ffa0b30c0a591b238e145fb020afc65ab9e5a14
Nelson Andrés Mora López <89644045+naml14@users.noreply.github.com>
2023-10-23 12:30:59 -0500

    Update Navbar.jsx
    error en el cierre de tags

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,8 +38,8 @@ function ColorSchemesExample() {
 
                     <Nav>
                         <NavDropdown title="Options" id="basic-nav-dropdown">
-                            <NavDropdown.Item><Link to='login'>Change User</Link>></></NavDropdown.Item>
-                            <NavDropdown.Item><Link to="login">Logout</Link>></></NavDropdown.Item>
+                            <NavDropdown.Item ><Link to='login'>Change User</Link></NavDropdown.Item>
+                            <NavDropdown.Item ><Link to="login">Logout</Link></NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Container>
